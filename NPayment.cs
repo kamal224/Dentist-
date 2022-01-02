98c2bd2b83d59df94df6eab27b402c696a81e520
Kamal Murshed <58914585+kamal224@users.noreply.github.com>
2022-01-02 11:40:49 +0300

    Add files via upload

---
+++ NPayment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Dentist
+{
+    public partial class NPayment : Form
+    {
+        public NPayment(int VID)
+        {
+            InitializeComponent();
+        }
+
+        private void NPayment_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
